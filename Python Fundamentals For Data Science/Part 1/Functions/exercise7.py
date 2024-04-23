93e11826c9b72811c4d5fd3e8847f7c7bbe06862
tlotlegomolale <144572551+tlotlegomolale@users.noreply.github.com>
2024-04-23 12:57:38 +0200

    Create exercise7.py

---
+++ Python Fundamentals For Data Science/Part 1/Functions/exercise7.py
@@ -0,0 +1,18 @@
+tshirt_price = 9.75
+shorts_price = 15.50
+mug_price = 5.99
+poster_price = 2.00
+
+# Write your code below:
+
+# Checkpoint 1
+max_price = max(tshirt_price, shorts_price, mug_price, poster_price)
+print(max_price)
+
+# Checkpoint 2
+min_price = min(tshirt_price, shorts_price, mug_price, poster_price)
+print(min_price)
+
+# Checkpoint 3
+rounded_price = round(tshirt_price, 1)
+print(rounded_price)
