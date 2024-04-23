11590d4325f109836cd70742f276a2adb3d66062
tlotlegomolale <144572551+tlotlegomolale@users.noreply.github.com>
2024-04-23 12:37:59 +0200

    Create exercise4.py

---
+++ Python Fundamentals For Data Science/Part 1/Functions/exercise4.py
@@ -0,0 +1,10 @@
+# Your code below:
+
+# Checkpoints 1, 2, & 3
+def generate_trip_instructions(location):
+  print("Looks like you are planning a trip to visit " + location)
+  print("You can use the public subway system to get to " + location)
+
+# Checkpoints 4 & 5
+#generate_trip_instructions("Central Park")
+generate_trip_instructions("Grand Central Station")
