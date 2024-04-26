9dea55fb085a49d9cf4d8063917bbde42e7f1775
tlotlegomolale <144572551+tlotlegomolale@users.noreply.github.com>
2024-04-26 06:58:32 +0200

    Create exercise11.py

---
+++ Python Fundamentals For Data Science/Part 1/Lists/exercise11.py
@@ -0,0 +1,13 @@
+#Your code below: 
+
+#Checkpoint 1
+class_name_test = [["Jenny", 90], ["Alexus", 85.5], ["Sam", 83], ["Ellie", 101.5]]
+print(class_name_test)
+
+#Checkpoint 2
+sams_score = class_name_test[2][1]
+print(sams_score)
+
+#Checkpoint 3
+ellies_score = class_name_test[-1][-1]
+print(ellies_score)
