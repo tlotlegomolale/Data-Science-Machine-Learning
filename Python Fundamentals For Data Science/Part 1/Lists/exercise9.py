7a90752b7742542c37bc6e786397b9c7f77ba824
tlotlegomolale <144572551+tlotlegomolale@users.noreply.github.com>
2024-04-26 06:33:00 +0200

    Create exercise9.py

---
+++ Python Fundamentals For Data Science/Part 1/Lists/exercise9.py
@@ -0,0 +1,23 @@
+#Your code below: 
+
+# Checkpoint 1
+order_list = ["Celery", "Orange Juice", "Orange", "Flatbread"]
+print(order_list)
+
+# Checkpoint 2
+order_list.remove("Flatbread")
+print(order_list)
+
+# Checkpoint 3
+new_store_order_list = ["Orange", "Apple", "Mango", "Broccoli", "Mango"]
+print(new_store_order_list)
+
+# Checkpoint 4
+new_store_order_list.remove("Mango")
+print(new_store_order_list)
+
+# Checkpoint 5
+new_store_order_list.remove("Onions")
+
+
+
