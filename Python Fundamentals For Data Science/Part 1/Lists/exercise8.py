6df379687581cd7172cc1bfe6826df4e916ffb4a
tlotlegomolale <144572551+tlotlegomolale@users.noreply.github.com>
2024-04-26 06:26:39 +0200

    Create exercise8.py

---
+++ Python Fundamentals For Data Science/Part 1/Lists/exercise8.py
@@ -0,0 +1,10 @@
+# Your code below:
+garden_waitlist = ["Jiho", "Adam", "Sonny", "Alisha"]
+
+garden_waitlist[1] = "Calla"
+
+print(garden_waitlist)
+
+garden_waitlist[-1] = "Alex"
+
+print(garden_waitlist)
