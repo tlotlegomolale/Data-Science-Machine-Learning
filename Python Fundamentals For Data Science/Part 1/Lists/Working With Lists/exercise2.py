fc882d8d273eb4c8995d71363f138121a5e94183
Tlotlego Lethabo Molale <144572551+tlotlegomolale@users.noreply.github.com>
2024-04-28 20:00:43 +0200

    Create exercise2.py

---
+++ Python Fundamentals For Data Science/Part 1/Lists/Working With Lists/exercise2.py
@@ -0,0 +1,15 @@
+data_science_topics = ["Machine Learning", "SQL", "Pandas", "Algorithms", "Statistics", "Python 3"]
+print(data_science_topics)
+
+# Your code below: 
+
+# Checkpoint 2
+data_science_topics.pop()
+print(data_science_topics)
+
+# Checkpoint 3
+data_science_topics.pop(3)
+print(data_science_topics)
+
+
+
