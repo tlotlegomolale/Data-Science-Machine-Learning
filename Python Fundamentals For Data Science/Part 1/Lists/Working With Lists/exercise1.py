28f9271cd67ee8d5c77ce477814f2f2acffb92c4
Tlotlego Lethabo Molale <144572551+tlotlegomolale@users.noreply.github.com>
2024-04-28 19:44:00 +0200

    Create exercise1.py

---
+++ Python Fundamentals For Data Science/Part 1/Lists/Working With Lists/exercise1.py
@@ -0,0 +1,8 @@
+front_display_list = ["Mango", "Filet Mignon", "Chocolate Milk"]
+print(front_display_list)
+
+# Your code below: 
+
+# Checkpoint 2
+front_display_list.insert(0, "Pineapple")
+print(front_display_list)
