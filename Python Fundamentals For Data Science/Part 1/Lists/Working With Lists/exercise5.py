26836ed5a899a0a6b979efc00e0b4d8bf8c0cc5a
Tlotlego Lethabo Molale <144572551+tlotlegomolale@users.noreply.github.com>
2024-04-29 03:53:24 +0200

    Create exercise5.py

---
+++ Python Fundamentals For Data Science/Part 1/Lists/Working With Lists/exercise5.py
@@ -0,0 +1,16 @@
+long_list = [1, 5, 6, 7, -23, 69.5, True, "very", "long", "list", "that", "keeps", "going.", "Let's", "practice", "getting", "the", "length"]
+
+big_range = range(2, 3000, 10)
+
+# Your code below: 
+
+# Checkpoint 1 & 2
+long_list_len = len(long_list)
+print(long_list_len)
+
+# Checkpoint 3 & 4
+big_range_length = len(big_range)
+print(big_range_length)
+
+# Checkpoint 5
+# Change range_list to be equal to range(2, 3000, 100)
