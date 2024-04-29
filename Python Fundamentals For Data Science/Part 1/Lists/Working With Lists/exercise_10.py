8ef07303684e30f39f0088086d3918876d791dfa
Tlotlego Lethabo Molale <144572551+tlotlegomolale@users.noreply.github.com>
2024-04-29 04:31:51 +0200

    Create exercise_10.py

---
+++ Python Fundamentals For Data Science/Part 1/Lists/Working With Lists/exercise_10.py
@@ -0,0 +1,9 @@
+games = ["Portal", "Minecraft", "Pacman", "Tetris", "The Sims", "Pokemon"]
+
+# Your code below:
+# Checkpoint 1
+games_sorted = sorted(games)
+
+# Checkpoint 2
+print(games)
+print(games_sorted)
