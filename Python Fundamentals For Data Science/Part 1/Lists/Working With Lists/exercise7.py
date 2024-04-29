651e6beafbeb4c1b6f6af457bc50b0d4f1d08945
Tlotlego Lethabo Molale <144572551+tlotlegomolale@users.noreply.github.com>
2024-04-29 04:06:19 +0200

    Create exercise7.py

---
+++ Python Fundamentals For Data Science/Part 1/Lists/Working With Lists/exercise7.py
@@ -0,0 +1,11 @@
+suitcase = ["shirt", "shirt", "pants", "pants", "pajamas", "books"]
+
+# Your code below: 
+
+# Checkpoint 1
+last_two_elements = suitcase[-2:]
+print(last_two_elements)
+
+# Checkpoint 2
+slice_off_last_three = suitcase[:-3]
+print(slice_off_last_three)
