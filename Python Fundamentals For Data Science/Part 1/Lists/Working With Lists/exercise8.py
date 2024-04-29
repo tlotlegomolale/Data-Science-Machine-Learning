4505ef12302db242c5fac9ebadb8d26c5facaa89
Tlotlego Lethabo Molale <144572551+tlotlegomolale@users.noreply.github.com>
2024-04-29 04:17:41 +0200

    Create exercise8.py

---
+++ Python Fundamentals For Data Science/Part 1/Lists/Working With Lists/exercise8.py
@@ -0,0 +1,9 @@
+votes = ["Jake", "Jake", "Laurie", "Laurie", "Laurie", "Jake", "Jake", "Jake", "Laurie", "Cassie", "Cassie", "Jake", "Jake", "Cassie", "Laurie", "Cassie", "Jake", "Jake", "Cassie", "Laurie"]
+
+# Your code below: 
+
+# Checkpoint 1
+jake_votes = votes.count("Jake")
+
+# Checkpoint 2
+print(jake_votes)
