2db655c8a5400d964f78130116c215d6a09fde95
Tlotlego Lethabo Molale <144572551+tlotlegomolale@users.noreply.github.com>
2024-05-08 12:54:59 +0200

    Create exercise3.py

---
+++ Python Fundamentals For Data Science/Part 2/Strings/exercise3.py
@@ -0,0 +1,8 @@
+first_name = "Reiko"
+last_name = "Matsuki"
+
+def password_generator(first_name, last_name):
+  temp_password = first_name[len(first_name)-3:] + last_name[len(last_name)-3:]
+  return temp_password
+
+temp_password = password_generator(first_name, last_name)
