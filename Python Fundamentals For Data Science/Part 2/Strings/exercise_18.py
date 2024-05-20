601ac352820b0d6ae9466c624b57b35b240ea7c6
Tlotlego Lethabo Molale <144572551+tlotlegomolale@users.noreply.github.com>
2024-05-20 21:33:02 +0200

    Create exercise_18.py

---
+++ Python Fundamentals For Data Science/Part 2/Strings/exercise_18.py
@@ -0,0 +1,5 @@
+def poem_title_card(title, poet):
+  poem_desc = "The poem \"{}\" is written by {}.".format(title, poet)
+  return poem_desc
+
+print(poem_title_card("I Hear America Singing","Walt Whitman"))
