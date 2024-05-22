04ccce051517211eae3f91d7cb908182e2cebd99
Tlotlego Lethabo Molale <144572551+tlotlegomolale@users.noreply.github.com>
2024-05-22 18:49:04 +0200

    Create exercise8.py

---
+++ Python Fundamentals For Data Science/Part 2/Dictionaries/exercise8.py
@@ -0,0 +1,6 @@
+zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"], "earth": ["Taurus", "Virgo", "Capricorn"], "air":["Gemini", "Libra", "Aquarius"]}
+
+zodiac_elements["energy"] = "Not a Zodiac element"
+
+if "energy" in zodiac_elements:
+  print(zodiac_elements["energy"])
