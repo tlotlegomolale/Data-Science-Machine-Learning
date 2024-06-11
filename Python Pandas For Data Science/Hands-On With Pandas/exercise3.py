dd5823fcba28e7293086dabc55f76f3bb55e8e84
Tlotlego Lethabo Molale <144572551+tlotlegomolale@users.noreply.github.com>
2024-06-11 08:37:44 +0200

    Create exercise3.py

---
+++ Python Pandas For Data Science/Hands-On With Pandas/exercise3.py
@@ -0,0 +1,4 @@
+name,cake_flavor,frosting_flavor,topping
+Chocolate Cake,chocolate,chocolate,chocolate shavings
+Birthday Cake,vanilla,vanilla,rainbow sprinkles
+Carrot cake,carrot,cream cheese,almonds
