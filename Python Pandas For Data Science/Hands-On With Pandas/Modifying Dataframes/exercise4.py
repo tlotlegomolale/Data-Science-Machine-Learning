c11918f9c10ed9ab9b9d976c67631859d1be4bf3
Tlotlego Lethabo Molale <144572551+tlotlegomolale@users.noreply.github.com>
2024-06-13 16:49:02 +0200

    Create exercise4.py

---
+++ Python Pandas For Data Science/Hands-On With Pandas/Modifying Dataframes/exercise4.py
@@ -0,0 +1,13 @@
+import codecademylib3
+import pandas as pd
+
+df = pd.DataFrame([
+  ['JOHN SMITH', 'john.smith@gmail.com'],
+  ['Jane Doe', 'jdoe@yahoo.com'],
+  ['joe schmo', 'joeschmo@hotmail.com']
+],
+columns=['Name', 'Email'])
+
+df['Lowercase Name'] = df.Name.apply(str.lower)
+
+print(df)
