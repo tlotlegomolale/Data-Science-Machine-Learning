6df6f41fa365b7fb096587fe4bb1848d3b0b63a3
Tlotlego Lethabo Molale <144572551+tlotlegomolale@users.noreply.github.com>
2024-06-17 14:48:32 +0200

    Create exercise2.py

---
+++ Python Pandas For Data Science/Aggregates In Pandas/exercise2.py
@@ -0,0 +1,10 @@
+import codecademylib3
+import pandas as pd
+
+orders = pd.read_csv('orders.csv')
+
+pricey_shoes = orders.groupby('shoe_type').price.max()
+
+print(pricey_shoes)
+
+print(type(pricey_shoes))
