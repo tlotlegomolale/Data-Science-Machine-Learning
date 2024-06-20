dfa2c2de6be246f48d85eb839aebb3d9101a4b32
Tlotlego Lethabo Molale <144572551+tlotlegomolale@users.noreply.github.com>
2024-06-20 14:02:19 +0200

    Update exercise1.py

--- Python Pandas For Data Science/Working With Multiple Tables/exercise1.py
+++ Python Pandas For Data Science/Working With Multiple Tables/exercise1.py
@@ -1,1 +1,16 @@
+import codecademylib3
+import pandas as pd
+
+orders = pd.read_csv('orders.csv')
+
+products = pd.read_csv('products.csv')
+
+customers = pd.read_csv('customers.csv')
+
+print(orders)
+print(products)
+print(customers)
+
+order_3_description = 'thing-a-ma-jig'
+order_5_phone_number = '112-358-1321'
 
