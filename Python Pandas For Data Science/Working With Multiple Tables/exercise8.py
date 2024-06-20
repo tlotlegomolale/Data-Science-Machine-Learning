7cb054982d25acd73cac445e0c0dd00e14c77376
Tlotlego Lethabo Molale <144572551+tlotlegomolale@users.noreply.github.com>
2024-06-20 16:32:33 +0200

    Create exercise8.py

---
+++ Python Pandas For Data Science/Working With Multiple Tables/exercise8.py
@@ -0,0 +1,13 @@
+import codecademylib3
+import pandas as pd
+
+store_a = pd.read_csv('store_a.csv')
+print(store_a)
+store_b = pd.read_csv('store_b.csv')
+print(store_b)
+
+store_a_b_left = pd.merge(store_a, store_b, how='left')
+store_b_a_left = pd.merge(store_b, store_a, how='left')
+
+print(store_a_b_left)
+print(store_b_a_left)
