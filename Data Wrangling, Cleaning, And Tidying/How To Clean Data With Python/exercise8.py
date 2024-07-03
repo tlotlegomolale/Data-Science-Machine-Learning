f6d8bb9641d7bb1dc76644f2393f20eb76464010
Tlotlego Lethabo Molale <144572551+tlotlegomolale@users.noreply.github.com>
2024-07-03 23:43:27 +0200

    Create exercise8.py

---
+++ Data Wrangling, Cleaning, And Tidying/How To Clean Data With Python/exercise8.py
@@ -0,0 +1,8 @@
+import codecademylib3_seaborn
+import pandas as pd
+from students import students
+
+students.score = students['score'].replace('[\%,]', '', regex=True)
+students.score = pd.to_numeric(students['score'])
+
+print(students.score[0])
