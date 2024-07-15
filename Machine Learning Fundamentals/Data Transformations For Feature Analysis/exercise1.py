4d450ab8ca9c4554aab2dc0e0b27c4f2f61776db
Tlotlego Lethabo Molale <144572551+tlotlegomolale@users.noreply.github.com>
2024-07-15 12:21:56 +0200

    Update exercise1.py

--- Machine Learning Fundamentals/Data Transformations For Feature Analysis/exercise1.py
+++ Machine Learning Fundamentals/Data Transformations For Feature Analysis/exercise1.py
@@ -1,1 +1,11 @@
+import pandas as pd 
 
+## add code below
+## set up dataframe
+coffee = pd.read_csv('starbucks_customers.csv')
+
+## print the column names
+print(coffee.columns)
+
+## get information on your data
+print(coffee.info())
