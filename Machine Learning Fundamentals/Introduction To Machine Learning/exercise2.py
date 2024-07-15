2a4a55393fce1796fbd050bd815b27f4547ff742
Tlotlego Lethabo Molale <144572551+tlotlegomolale@users.noreply.github.com>
2024-07-15 11:47:54 +0200

    Create exercise2.py

---
+++ Machine Learning Fundamentals/Introduction To Machine Learning/exercise2.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+
+# Load the data
+photo_id_times = pd.read_csv('photo_id_times.csv')
+
+# Separate the data into independent and dependent variables
+X = np.array(photo_id_times['Time to id photo']).reshape(-1, 1)
+y = photo_id_times['Class']
+
+# Create a model and fit it to the data
+neigh = KNeighborsClassifier(n_neighbors=3)
+neigh.fit(X, y)
+
+time_to_identify_picture = 6.5
+
+# Make a prediction based on how long it takes to identify a picture
+y_pred = neigh.predict(np.array(time_to_identify_picture).reshape(1, -1))
+
+if y_pred == 1:
+    print("We think you're a robot.")
+else:
+    print("Welcome, human!")
