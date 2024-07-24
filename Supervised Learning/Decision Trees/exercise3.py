ffdece353a3d13744fc5de3aca5cf088099eeccb
Tlotlego Lethabo Molale <144572551+tlotlegomolale@users.noreply.github.com>
2024-07-24 13:33:14 +0200

    Create exercise3.py

---
+++ Supervised Learning/Decision Trees/exercise3.py
@@ -0,0 +1,7 @@
+
+gini_root = 1 - ( (412/1382)**2 + (970/1382)**2 )
+print(f'Gini impurity root node : {gini_root}')
+
+
+gini_bottom_left = 1 - ( (96/449)**2 + (353/449)**2 )
+print(f'Gini impurity bottom left node : {gini_bottom_left}')
