3cc93d5092caccde1c963e160b78c0d900724f38
Tlotlego Lethabo Molale <144572551+tlotlegomolale@users.noreply.github.com>
2024-07-24 13:21:15 +0200

    Create exercise2.py

---
+++ Supervised Learning/Decision Trees/exercise2.py
@@ -0,0 +1,6 @@
+frac_acc = 412/1382
+print(f'Fraction of acceptable cars: {frac_acc}')
+low_safety_class = 'unacc'
+print(f'Cars with low safety: {low_safety_class}')
+sample_class = 'unacc'
+print(f'The class of the sample car: {sample_class}')
