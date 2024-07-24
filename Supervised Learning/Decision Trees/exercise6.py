910d38f6d301796cca755307d265947c81c0d0f8
Tlotlego Lethabo Molale <144572551+tlotlegomolale@users.noreply.github.com>
2024-07-24 15:08:43 +0200

    Create exercise6.py

---
+++ Supervised Learning/Decision Trees/exercise6.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+
+def gini(data):
+    """calculate the Gini Impurity
+    """
+    data = pd.Series(data)
+    return 1 - sum(data.value_counts(normalize=True)**2)
+   
+def info_gain(left, right, current_impurity):
+    """Information Gain associated with creating a node/split data.
+    Input: left, right are data in left branch, right banch, respectively
+    current_impurity is the data impurity before splitting into left, right branches
+    """
+    # weight for gini score of the left branch
+    w = float(len(left)) / (len(left) + len(right))
+    return current_impurity - w * gini(left) - (1 - w) * gini(right)
+
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data', names=['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety', 'accep'])
+df['accep'] = ~(df['accep']=='unacc') #1 is acceptable, 0 if not acceptable
+X = pd.get_dummies(df.iloc[:,0:6])
+y = df['accep']
+
+x_train, x_test, y_train, y_test = train_test_split(X,y, random_state=0, test_size=0.2)
+
+y_train_sub = y_train[x_train['safety_low']==0]
+x_train_sub = x_train[x_train['safety_low']==0]
+
+gi = gini(y_train_sub)
+print(f'Gini impurity at root: {gi}')
+
+info_gain_list = []
+for i in x_train.columns:
+    left = y_train_sub[x_train_sub[i]==0]
+    right = y_train_sub[x_train_sub[i]==1]
+    info_gain_list.append([i, info_gain(left, right, gi)])
+
+info_gain_table = pd.DataFrame(info_gain_list).sort_values(1,ascending=False)
+print(f'Greatest impurity gain at:{info_gain_table.iloc[0,:]}')
