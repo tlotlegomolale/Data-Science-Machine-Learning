ddedce0dead9568d4dbbbcd2cb11bbb78927610c
Tlotlego Lethabo Molale <144572551+tlotlegomolale@users.noreply.github.com>
2024-07-25 11:05:05 +0200

    Update exercise1.py

--- Supervised Learning/Evaluation metrics/exercise1.py
+++ Supervised Learning/Evaluation metrics/exercise1.py
@@ -1,1 +1,25 @@
+from sklearn.metrics import confusion_matrix
 
+actual = [1, 0, 0, 1, 1, 1, 0, 1, 1, 1]
+predicted = [0, 1, 1, 1, 1, 0, 1, 0, 1, 0]
+
+true_positives = 0
+true_negatives = 0
+false_positives = 0
+false_negatives = 0
+
+for i in range(len(predicted)):
+  if actual[i] == 1 and predicted[i] == 1:
+    true_positives += 1
+  if actual[i] == 0 and predicted[i] == 0:
+    true_negatives += 1
+  if actual[i] == 0 and predicted[i] == 1:
+    false_positives += 1
+  if actual[i] == 1 and predicted[i] == 0:
+    false_negatives += 1
+
+print(true_positives, true_negatives, false_positives, false_negatives)
+
+conf_matrix = confusion_matrix(actual, predicted)
+
+print(conf_matrix)
