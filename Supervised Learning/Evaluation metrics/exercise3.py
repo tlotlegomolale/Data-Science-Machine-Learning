54610e6cdd67e09b92b0194bc66b128c0d01f7bf
Tlotlego Lethabo Molale <144572551+tlotlegomolale@users.noreply.github.com>
2024-07-25 12:08:42 +0200

    Create exercise3.py

---
+++ Supervised Learning/Evaluation metrics/exercise3.py
@@ -0,0 +1,21 @@
+actual = [1, 0, 0, 1, 1, 1, 0, 1, 1, 1]
+predicted = [0, 1, 1, 1, 1, 0, 1, 0, 1, 0]
+
+true_positives = 0
+true_negatives = 0
+false_positives = 0
+false_negatives = 0
+
+for i in range(len(predicted)):
+  if actual[i] == 1 and predicted[i] == 1:
+    true_positives += 1
+  if actual[i] == 0 and predicted[i] == 0:
+    true_negatives += 1
+  if actual[i] == 0 and predicted[i] == 1:
+    false_positives += 1
+  if actual[i] == 1 and predicted[i] == 0:
+    false_negatives += 1
+
+recall = true_positives/(true_positives + false_negatives)
+
+print(recall)
