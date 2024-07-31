6da2790e0f6813644e193e31187985c16d9eb874
Tlotlego Lethabo Molale <144572551+tlotlegomolale@users.noreply.github.com>
2024-07-31 18:42:32 +0200

    Create exercise2.py

---
+++ Unsupervised Learning/Principal Component Analysis/exercise2.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import codecademylib3
+
+
+data_matrix = pd.read_csv('./data_matrix.csv')
+
+# 1. Use the `.corr()` method on `data_matrix` to get the correlation matrix 
+correlation_matrix = data_matrix.corr()
+
+## Heatmap code:
+red_blue = sns.diverging_palette(220, 20, as_cmap=True)
+sns.heatmap(correlation_matrix, vmin = -1, vmax = 1, cmap=red_blue)
+plt.show()
+
+# 2. Perform eigendecomposition using `np.linalg.eig` 
+eigenvalues, eigenvectors = np.linalg.eig(correlation_matrix)
+
+# 3. Print out the eigenvectors and eigenvalues
+print('eigenvectors: ')
+print(eigenvectors)
+
+print('eigenvalues: ')
+print(eigenvalues)
+
