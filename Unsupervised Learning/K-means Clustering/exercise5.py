9504b56b7a4b0fd77d66601597780e5cf2e45050
Tlotlego Lethabo Molale <144572551+tlotlegomolale@users.noreply.github.com>
2024-07-31 14:38:22 +0200

    Create exercise5.py

---
+++ Unsupervised Learning/K-means Clustering/exercise5.py
@@ -0,0 +1,64 @@
+import codecademylib3_seaborn
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets
+from copy import deepcopy
+
+iris = datasets.load_iris()
+
+samples = iris.data
+
+x = samples[:,0]
+y = samples[:,1]
+
+sepal_length_width = np.array(list(zip(x, y)))
+
+# Step 1: Place K random centroids
+
+k = 3
+
+centroids_x = np.random.uniform(min(x), max(x), size=k)
+centroids_y = np.random.uniform(min(y), max(y), size=k)
+
+centroids = np.array(list(zip(centroids_x, centroids_y)))
+
+# Step 2: Assign samples to nearest centroid
+
+# Distance formula
+
+def distance(a, b):
+  one = (a[0] - b[0]) **2
+  two = (a[1] - b[1]) **2
+  distance = (one+two) ** 0.5
+  return distance
+
+# Cluster labels for each point (either 0, 1, or 2)
+
+labels = np.zeros(len(samples))
+
+# A function that assigns the nearest centroid to a sample
+
+def assign_to_centroid(sample, centroids):
+  k = len(centroids)
+  distances = np.zeros(k)
+  for i in range(k):
+    distances[i] = distance(sample, centroids[i])
+  closest_centroid = np.argmin(distances)
+  return closest_centroid
+
+# Assign the nearest centroid to each sample
+
+for i in range(len(samples)):
+  labels[i] = assign_to_centroid(samples[i], centroids)
+
+# Step 3: Update centroids
+
+centroids_old = deepcopy(centroids)
+
+for i in range(k):
+  points = [sepal_length_width[j] for j in range(len(sepal_length_width)) if labels[j] == i]
+  centroids[i] = np.mean(points, axis=0)
+  
+print(centroids_old)
+print("- - - - - - - - - - - - - -")
+print(centroids)
